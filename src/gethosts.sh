72a0fd6e1c07b3e37e48e067f8f0acee9816bac3
samsepi0x0 <64140687+samsepi0x0@users.noreply.github.com>
2022-09-06 20:56:56 +0530

    Add files via upload

--- src/gethosts.sh
+++ src/gethosts.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # MIT License
-# @Agni Datta 2022, @Nadir 2022
+# @Agni Datta 2022, @Nadir Khan 2022
 # This script downlaods the latest Steven Black hosts file from Github Mirror.
 # There is a backup and move component which moves the downlaoded hosts file to /etc/hosts
 # Script shall be stored in /etc/init.d/gethosts.sh
@@ -14,6 +14,8 @@ while true; do
       curl https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts -o /tmp/hosts | grep "HasErrors.:true"
       sudo mv /etc/hosts /etc/hosts.old
       sudo cp /tmp/hosts /etc/hosts | grep "HasErrors.:true"
+      sudo chmod u=r-- /etc/hosts
+      sudo chmod o=--- /etc/hosts
       echo "Script ran sucessfully at " date "!" >> "customhostslogfile.$(date +'%Y-%m-%d').log"
       if [[ "$?" -eq 0 ]]; then
 	  echo "SCRIPT RAN"
